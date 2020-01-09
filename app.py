d72f3c90051472bb5e958b6da94462d3704fb6d8
erdincozsertel <erdincosertel1@hotmail.com>
2020-01-09 10:56:24 +0300

    last commit

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ app.title = "NYC Taxi Data"
 
 colors = {
     'background': '#6F884C',
-    'text': '#131311'
+    'text': '#131311',
+    'graph_background': '#ffffff'
 }
 
 
@@ -41,47 +42,6 @@ df['pickup_minutes'] = df['tpep_pickup_datetime'].dt.minute
 df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
 df['dropoff_hours'] = df['tpep_dropoff_datetime'].dt.hour
 df['dropoff_minutes'] = df['tpep_dropoff_datetime'].dt.minute
-
-
-
-# pLat=df['pickup_latitude'].values.tolist()
-# pLong=df['pickup_longitude'].values.tolist()
-
-# pDCor=[]
-
-# dfsize = (df.size).astype(int)-1
-# #dfsize = df.size.to_datetime
-
-
-# for i in range(dfsize):
-#     pDCoor = (pLat[i], pLong[i])
-#     pDCor.append(pDCoor)
-
-# pDNames = rg.search(pDCor)
-
-# for i in range(dfsize):
-#     pDName = (pDNames[i])['name']
-#     pDNames[i]=pDName    
-
-# df['pickup_district']=pDNames
-
-
-# dLat=df['dropoff_latitude']
-# dLong=df['dropoff_longitude']
-
-# dDCor=[]
-
-# for i in range(dfsize):
-#     dDCoor = (dLat[i], dLong[i])
-#     dDCor.append(dDCoor)
-
-# dDNames = rg.search(dDCor)
-
-# for i in range(dfsize):
-#     dDName = (dDNames[i])['name']
-#     dDNames[i]=dDName
-
-# df['dropoff_district']=dDNames
 
 
 
@@ -117,7 +77,7 @@ layout_table = dict(
         t=45
     ),
     hovermode="closest",
-    plot_bgcolor=colors['background'],
+    plot_bgcolor=colors['graph_background'],
     paper_bgcolor=colors['background'],
     legend=dict(font=dict(size=10), orientation='h'),
 )
@@ -138,7 +98,7 @@ layout_map = dict(
         t=45
     ),
     hovermode="closest",
-    plot_bgcolor=colors['background'],
+    plot_bgcolor=colors['graph_background'],
     paper_bgcolor=colors['background'],
     legend=dict(font=dict(size=10), orientation='h'),
     title='New York City Pickup Locations',
@@ -156,19 +116,26 @@ layout_map = dict(
 xData1 = []
 yData1 = []
 
-for group, data in df.groupby(['dropoff_hours']):
+for group, data in dfOriginal.groupby(['dropoff_hours']):
     xData1.append(str(group) + ":" + "00")
     yData1.append(data['total_amount'].sum())
 
+xData2 = []
+yData2 = []
+
+for group, data in dfOriginal.groupby(['pickup_district']):
+    xData2.append(str(group))
+    yData2.append(data['total_amount'].sum())
+
   
 trace_pickup = go.Scattermapbox(
-        lat= df['pickup_latitude'],
-        lon= df['pickup_longitude'],
+        lat= dfOriginal['pickup_latitude'],
+        lon= dfOriginal['pickup_longitude'],
 		mode= "markers",
         marker= dict(size=6, color='#51884C', opacity=0.7),
         hoverinfo= "text",
-        hovertext= [["PassengerCount: {} <br>Pickup Time: {} <br>Vendor: {}<br>Price: {}".format(i,j,k,l)]
-        for i,j,k,l in zip(dfOriginal['passenger_count'].apply(str), dfOriginal['pickup_hours'].apply(str)+" : "+dfOriginal['pickup_minutes'].apply(str),dfOriginal['VendorID'].apply(str),dfOriginal['total_amount'].apply(str))],
+        hovertext= [["PassengerCount: {} <br>Pickup Time: {} <br>Vendor: {} <br>Price: {} <br>District: {}".format(i,j,k,l,m)]
+        for i,j,k,l,m in zip(dfOriginal['passenger_count'].apply(str), dfOriginal['pickup_hours'].apply(str)+" : "+dfOriginal['pickup_minutes'].apply(str),dfOriginal['VendorID'].apply(str),dfOriginal['total_amount'].apply(str),dfOriginal['pickup_district'].apply(str) )],
         #layout = layout_map,
 
   )
@@ -210,7 +177,7 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     ),
     #selector
     html.Div(
-        className="four columns div-user-controls",
+        className="row",
         children=[
             html.Div(
                 className="div-for-dropdown",
@@ -219,7 +186,7 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
                         id="time-selector",
                         options=[
                             {"label":str(i)+" : 00","value":i}
-                            for i in dfOriginal['dropoff_hours'].unique()
+                            for i in range(24)
                         ],
                         multi=False,
                         searchable = False,
@@ -228,8 +195,8 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
                     dcc.Dropdown(
                         id="vendor-selector",
                         options=[
-                            {"label":str(i),"value":i}
-                            for i in dfOriginal['VendorID'].unique()
+                            {"label":"Vendor "+str(i),"value":i}
+                            for i in range(1,3)
                         ],
                         multi=False,
                         searchable = False,
@@ -242,20 +209,20 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     ),
 
     #Graph 1 head
-    html.Div(children='Income per Hour.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
+    # html.Div(children='Income per Hour.', style={
+    #     'textAlign': 'center',
+    #     'color': colors['text']
+    # }),
     #Graph 1
     dcc.Graph(
         id='graph1',
 
     ),
 	#Graph 2 head
-	html.Div(children='Income per District.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
+	# html.Div(children='Income per District.', style={
+    #     'textAlign': 'center',
+    #     'color': colors['text']
+    # }),
 	#Graph 2
 	dcc.Graph(
         id='graph2',
@@ -269,14 +236,60 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
 )
 def map_update(vendor, time):
     if vendor is not None:
+        dfMap = dfOriginal[dfOriginal.VendorID == vendor]
+        
         if time is not None:
-            pass
+            dfMap = dfMap[dfMap.pickup_hours == time]
+            trace_vt=go.Scattermapbox(
+                lat = dfMap['pickup_latitude'],
+                lon = dfMap['pickup_longitude'],
+                mode = "markers",
+
+                marker= dict(size=6, color='#51884C', opacity=0.7),
+
+                hoverinfo= "text",
+
+                hovertext= [["PassengerCount: {} <br>Pickup Time: {} <br>Vendor: {} <br>Price: {} <br>District: {}".format(i,j,k,l,m)]
+                
+                for i,j,k,l,m in zip(dfOriginal['passenger_count'].apply(str), dfOriginal['pickup_hours'].apply(str)+" : "+dfOriginal['pickup_minutes'].apply(str),dfOriginal['VendorID'].apply(str),dfOriginal['total_amount'].apply(str),dfOriginal['pickup_district'].apply(str) )],
+
+            )
+            return{"data":[trace_vt],"layout":layout_map}
         else:
-            pass
+            trace_v=go.Scattermapbox(
+                lat = dfMap['pickup_latitude'],
+                lon = dfMap['pickup_longitude'],
+                mode = "markers",
+
+                marker= dict(size=6, color='#51884C', opacity=0.7),
+
+                hoverinfo= "text",
+
+                hovertext= [["PassengerCount: {} <br>Pickup Time: {} <br>Vendor: {} <br>Price: {} <br>District: {}".format(i,j,k,l,m)]
+                
+                for i,j,k,l,m in zip(dfOriginal['passenger_count'].apply(str), dfOriginal['pickup_hours'].apply(str)+" : "+dfOriginal['pickup_minutes'].apply(str),dfOriginal['VendorID'].apply(str),dfOriginal['total_amount'].apply(str),dfOriginal['pickup_district'].apply(str) )],
+
+            )
+            return{"data":[trace_v],"layout":layout_map}
 
     else:
         if time is not None:
-            pass
+            dfMap = dfMap[dfMap.pickup_hours == int(time)]
+            trace_t=go.Scattermapbox(
+                lat = dfMap['pickup_latitude'],
+                lon = dfMap['pickup_longitude'],
+                mode = "markers",
+
+                marker= dict(size=6, color='#51884C', opacity=0.7),
+
+                hoverinfo= "text",
+
+                hovertext= [["PassengerCount: {} <br>Pickup Time: {} <br>Vendor: {} <br>Price: {} <br>District: {}".format(i,j,k,l,m)]
+                
+                for i,j,k,l,m in zip(dfOriginal['passenger_count'].apply(str), dfOriginal['pickup_hours'].apply(str)+" : "+dfOriginal['pickup_minutes'].apply(str),dfOriginal['VendorID'].apply(str),dfOriginal['total_amount'].apply(str),dfOriginal['pickup_district'].apply(str) )],
+
+            )
+            return{"data":[trace_t],"layout":layout_map}
         else:
             return{"data":[trace_pickup],"layout":layout_map}
 
@@ -289,8 +302,6 @@ def map_update(vendor, time):
     [Input('vendor-selector','value')]
 )
 def graph1_update(vendor):
-    dfGraph1_1=dfOriginal[dfOriginal.VendorID==1]
-    dfGraph1_2=dfOriginal[dfOriginal.VendorID==2]
     
     df_1 = dfOriginal[dfOriginal.VendorID == 1]
     df_2 = dfOriginal[dfOriginal.VendorID == 2]
@@ -314,7 +325,7 @@ def graph1_update(vendor):
     figure_def={
         'data':[{'x':xData1,'y':yData1, 'type':'bar','marker':dict(color='#51884C'),'name':'hourIncome'}],
         'layout':{
-            'plot_bgcolor':colors['background'],
+            'plot_bgcolor':colors['graph_background'],
             'paper_bgcolor':colors['background'],
             'font':{
                 'color':colors['text'],
@@ -327,7 +338,7 @@ def graph1_update(vendor):
     figure_1={
         'data':[{'x':v1xData1,'y':v1yData1, 'type':'bar','marker':dict(color='#51884C'),'name':'hourIncome'}],
         'layout':{
-            'plot_bgcolor':colors['background'],
+            'plot_bgcolor':colors['graph_background'],
             'paper_bgcolor':colors['background'],
             'font':{
                 'color':colors['text'],
@@ -339,7 +350,7 @@ def graph1_update(vendor):
     figure_2={
         'data':[{'x':v2xData1,'y':v2yData1, 'type':'bar','marker':dict(color='#51884C'),'name':'hourIncome'}],
         'layout':{
-            'plot_bgcolor':colors['background'],
+            'plot_bgcolor':colors['graph_background'],
             'paper_bgcolor':colors['background'],
             'font':{
                 'color':colors['text'],
@@ -349,12 +360,64 @@ def graph1_update(vendor):
         }
     }
 
-    if vendor == '1':
+    if vendor == 1:
         return figure_1
-    elif vendor=='2':
+    elif vendor==2:
         return figure_2
     else:
         return figure_def
+
+### Graph 2: (All hours defult)Income per Vendor by district
+### Style: Line
+### Parameter: Hour
+### Chose: Vendor
+@app.callback(
+    Output('graph2','figure'),
+    [Input('time-selector','value')]
+)
+def graph2_update(time):
+
+    #51884C
+    figure_def={
+        'data':[{'x':xData2,'y':yData2, 'type':'line','marker':dict(color='#51884C'),'name':'hourIncome'}],
+        'layout':{
+            'plot_bgcolor':colors['graph_background'],
+            'paper_bgcolor':colors['background'],
+            'font':{
+                'color':colors['text'],
+            },
+            'titlefont':dict(color=colors['text'], size='14'),
+            'title':'Income per District.',
+        }
+    }
+
+    
+    if time is not None:
+        df_1 = dfOriginal[dfOriginal.pickup_hours == int(time)]
+        v1xData2 = []
+        v1yData2 = []
+
+    
+        for group, data in df_1.groupby(['pickup_district']):
+            v1xData2.append(str(group))
+            v1yData2.append(data['total_amount'].sum())
+
+        figure_1={
+            'data':[{'x':v1xData2,'y':v1yData2, 'type':'line','marker':dict(color='#51884C'),'name':'hourIncome'}],
+            'layout':{
+                'plot_bgcolor':colors['graph_background'],
+                'paper_bgcolor':colors['background'],
+                'font':{
+                    'color':colors['text'],
+                },
+                'titlefont':dict(color=colors['text'], size='14'),
+                'title':'Income per District.',
+            }
+        }
+        return figure_1
+    else:
+        return figure_def
+
 
 
 if __name__ == '__main__':
